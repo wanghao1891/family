c47565734f24f0a23338684913d54cf813fa1f02
wanghao1891 <wanghao1891@126.com>
2016-03-28 18:37:10 +0800

    Change the style of app.js to flat format.

--- client/app.jsx
+++ client/app.jsx
@@ -1,30 +1,36 @@
 // App component - represents the whole app
-App = React.createClass({
-  getFoods() {
-    return [
-      { _id: 1, text: "This is food 1" },
-      { _id: 2, text: "This is food 2" },
-      { _id: 3, text: "This is food 3" }
-    ];
-  },
+function get_foods() {
+  return [
+    { _id: 1, text: "This is food 1" },
+    { _id: 2, text: "This is food 2" },
+    { _id: 3, text: "This is food 3" }
+  ];
+}
 
-  renderFoods() {
-    return this.getFoods().map((food) => {
-      return <Food key={food._id} food={food} />;
-    });
-  },
+function render_foods() {
+  return this.get_foods().map((food) => {
+    return <Food key={food._id} food={food} />;
+  });
+}
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Food List</h1>
-        </header>
+function render() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Food List</h1>
+      </header>
 
-        <ul>
-          {this.renderFoods()}
-        </ul>
-      </div>
-    );
-  }
-});
+      <ul>
+        {this.render_foods()}
+      </ul>
+    </div>
+  );
+}
+
+var options = {
+  get_foods: get_foods,
+  render_foods: render_foods,
+  render: render
+};
+
+App = React.createClass(options);
